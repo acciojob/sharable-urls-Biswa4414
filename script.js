e91c3b407ae2e73d8a2221a735e7440ea23501c6
Biswa Saraf <117895834+Biswa4414@users.noreply.github.com>
2024-01-04 00:11:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 // your code here
+updateURL = () => {
+        const nameValue = document.getElementById("name").value;
+        const yearValue = document.getElementById("year").value;
+        const currentURL = document.getElementById("url").innerText;
+
+        let str = "";
+        if (nameValue !== "") {
+          str = str + "?name=" + encodeURIComponent(nameValue);
+        } else if (yearValue !== "") {
+          str = str + "?year=" + encodeURIComponent(yearValue);
+        }
+        const updateURL = currentURL + str;
+        document.getElementById("url").innerText = updateURL;
+      };
